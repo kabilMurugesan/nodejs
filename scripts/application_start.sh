cf37b59038695a8a286200811c96877738b1da89
kabilMurugesan <98404076+kabilMurugesan@users.noreply.github.com>
2024-05-08 16:27:48 +0530

    jsj

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /var/www/nodejs
-# pm2 delete server
-sudo pm2 start index.js
+sudo pm2 start index.js --name "nodejs"
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,2 @@
 # !/bin/bash
-cd /var/www/nodejs
-# sudo apt-get update
-# sudo pm2 delete all
-# sudo pm2 flush               
+cd /var/www/nodejs             
